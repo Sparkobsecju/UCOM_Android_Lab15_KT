09ad8f4a74ecaf4acc52cdfccad512c7702ac781
Sivan Chen <ryan1825114@gmail.com>
2024-06-27 13:54:39 +0800

    create an IntentService

--- app/src/main/java/com/example/lab15_kt/EchoIntentService.kt
+++ app/src/main/java/com/example/lab15_kt/EchoIntentService.kt
@@ -2,9 +2,25 @@ package com.example.lab15_kt
 
 import android.app.IntentService
 import android.content.Intent
+import android.util.Log
+import com.example.lab15_kt.EchoService.Companion.TAG
 
 class EchoIntentService: IntentService(EchoService::class.java.simpleName) {
     override fun onHandleIntent(intent: Intent?) {
         TODO("Not yet implemented")
+    }
+
+    private var flag = false
+
+    private fun doEcho(startId: Int) {
+        Thread{
+            for (count in 1..10) {
+                Log.v(TAG, "啟動的id: $startId, 計數: $count")
+                Thread.sleep(500)
+                if (flag) {
+                    break
+                }
+            }
+        }.start()
     }
 }
