44f38ed76d12a826b6ed2d342270a1c1cccd035e
Sivan Chen <ryan1825114@gmail.com>
2024-06-27 11:57:00 +0800

    create a Service

---
+++ app/src/main/java/com/example/lab15_kt/EchoService.kt
@@ -0,0 +1,12 @@
+package com.example.lab15_kt
+
+import android.app.Service
+import android.content.Intent
+import android.os.IBinder
+
+class EchoService: Service() {
+
+    override fun onBind(intent: Intent?): IBinder? {
+        TODO("Not yet implemented")
+    }
+}
